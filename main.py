e4197d7e06a5a2ec8a1cc0eb2281c063f3e48765
TheUnchosen <longbui369@gmail.com>
2024-06-29 18:52:29 +0000

    2222

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    while True:
+        try:
+            number = int(input("Enter a number "))
+        except ValueError:
+            print ("invalid input")
+            continue
+        else:
+            print(number)
+            break
 
     ########################################
     # Do not delete the return statement
